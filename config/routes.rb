7782582939b5e525e89ea9b01ecb58671529d02d
Susan Pan <susan.pan96@gmail.com>
2020-11-17 12:06:57 +1000

    view all tasks, create new task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id', to: 'tasks#show'
 end
